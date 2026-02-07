ad3f0ecf48633d1d40acdf6b5d7318cdfbda2721
Kartikey Rathore <160748134+kartik-op1@users.noreply.github.com>
2026-02-07 11:28:59 +0000

    Datatypes and ECMA Script

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+let name = "Kartikey";
+let age = 12;
+console.log(typeof name);
+
+
+console.log(typeof 34.57);
+console.table([typeof "Kartik", typeof 23,typeof null,typeof undefined ,typeof BigInt,typeof Boolean]);
+
+// Take Reference from ECMA Script
