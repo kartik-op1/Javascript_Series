334a2763d2f5c4e86e7901215befddf72c13d783
Kartikey Rathore <160748134+kartik-op1@users.noreply.github.com>
2026-02-07 09:25:31 +0000

    learning of variables and keyword done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 2332343;
+
+let accountEmail = "Kartikeyrathore2344@google,.com";
+
+var accountPassword = "34324";
+
+accountCity = "Jaipur";
+
+//accountId = 34334;
+console.table([accountEmail,accountPassword,accountCity]);
+accountEmail = "google@gmail.com";
+accountPassword = "r23434";
+accountCity = "Bangaluru";
+let accountState;
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
