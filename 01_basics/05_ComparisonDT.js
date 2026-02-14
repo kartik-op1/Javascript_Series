cd0b1d14ce9fe960263140d2e77640d83a2eb8d8
Kartikey Rathore <160748134+kartik-op1@users.noreply.github.com>
2026-02-14 09:10:46 +0000

    Comparison of DataTypes

---
+++ 01_basics/05_ComparisonDT.js
@@ -0,0 +1,44 @@
+// console.log(2 == 2);
+// console.log(2 == "2");
+// console.log(2 === "2");
+// console.log(2 != 2);
+// console.log(2 != "2");
+// console.log(2 !== "2");
+
+let a = 2;
+let b = "2";
+// console.log("2" > 1);
+// console.log("2" < 1);
+// console.log("2" >= 1);
+// console.log("2" <= 1);
+// console.log("2" == 2);
+
+
+let ans = undefined;
+console.log(ans);
+
+// console.log(undefined == null);
+// console.log(undefined === null);
+
+// console.log(undefined > null);
+// console.log(undefined < null);
+
+// console.log(undefined >= null);
+// console.log(undefined <= null);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+// console.log(undefined === 0);
+
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0);
+console.log(null === 0);
+
