7846af460505c25e2e90b50e23a9736494d56ae7
Kartikey Rathore <160748134+kartik-op1@users.noreply.github.com>
2026-02-14 08:47:37 +0000

    Arithmetic Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,38 @@
+let value = 2;
+let negativeValue = -value;
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let str = "Hello";
+let str2 = "World";
+console.log(str  + str2);
+
+console.log("strt" + "2");
+console.log("sfsrf" - "2");
+console.log("sfsrf" * "2");
+console.log("sfsrf" / "2");
+console.log("sfsrf" % "2");
+console.log("sfsrf" ** "2");
+
+
+console.log("strt" + 2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true + false);
+
+
+
+let num1,num2,num3;
+num1 = num2 = num3 = 5;
+console.log(num1,num2,num3);
+
+console.log(+true);
+console.log(+false);
