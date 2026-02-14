50a797c7064cd730c1871eef2b7a428b6d67d536
Kartikey Rathore <160748134+kartik-op1@users.noreply.github.com>
2026-02-14 08:26:49 +0000

    Add some More Conversion

--- 01_basics/03_Conversion.js
+++ 01_basics/03_Conversion.js
@@ -3,3 +3,27 @@ let valuINNumber = Number(name);
 console.log(typeof valuINNumber);
 console.log(valuINNumber);
 
+let name1 = "Kartikey";
+let convert = Number(name1);
+console.log(convert);
+console.log(typeof convert );
+ 
+let convert2 = Boolean(name1);
+// console.log(convert2);
+// console.log(typeof convert2);
+
+
+let name3 = "";
+let convert3 = Boolean(name3);
+
+console.log(convert3);
+
+let checked = undefined;
+let convert4 = Number(checked);
+console.log(convert4);
+
+
+
+
+
+
